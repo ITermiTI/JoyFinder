355bfff6cc50f13c9123eebc2375e86ec9431acf
Wojtek <wojciech.nokielski@gmail.com>
2020-05-13 21:31:20 +0200

    First version of addEvent view

--- Web/joy-finder-web/src/pages/AddEvent.js
+++ Web/joy-finder-web/src/pages/AddEvent.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import AddEventBox from '../components/AddEventBox.js'
 
 class AddEvent extends React.Component{
     render(){
         return (
             <div className="component-background">
                 <div className="home-page-title-text">Create your own event!</div>
+                <AddEventBox/>
             </div>   
             
         );
