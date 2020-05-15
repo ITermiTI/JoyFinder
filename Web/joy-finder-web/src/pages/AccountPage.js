91f0ac6ffadd3d50a78e1ee1f148140cc475d1f7
Wojtek <wojciech.nokielski@gmail.com>
2020-05-15 11:20:54 +0200

    Change password view

--- Web/joy-finder-web/src/pages/AccountPage.js
+++ Web/joy-finder-web/src/pages/AccountPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AccountPageBox from '../components/AccountPageBox.js'
-import EditAccountDetailsBox from '../components/EditAccountDetailsBox.js';
+import EditAccountDetailsBox from '../components/EditAccountDetailsBox.js'
+import ChangePasswordBox from '../components/ChangePasswordBox.js'
 
 class AccountPage extends React.Component{
 
@@ -20,6 +21,7 @@ class AccountPage extends React.Component{
             <div className="component-background">
                 <div className="home-page-title-text">Your Account!</div>
                 <button className="edit-details-button" onClick={this.handleClick.bind(this, 'edit')}>Edit details</button>
+                <button className="change-password-button" onClick={this.handleClick.bind(this, 'change')}>Change password</button>
                 <AccountPageBox/>
             </div>   
         );
@@ -28,6 +30,13 @@ class AccountPage extends React.Component{
             <div className="home-page-title-text">Edit your account</div>
             <button className="save-details-button" onClick={this.handleClick.bind(this, 'view')}>Save</button>
                 <EditAccountDetailsBox/>
+            </div>
+        );
+        if(this.state.render=='change') return(
+            <div className="component-background">
+            <div className="home-page-title-text">Change your password</div>
+            <button className="submit-button" onClick={this.handleClick.bind(this, 'view')}>Submit</button>
+                <ChangePasswordBox/>
             </div>
         );
     }
