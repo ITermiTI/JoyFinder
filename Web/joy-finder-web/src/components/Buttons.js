61123f879f2ff95d97fcc50984e5f142cda3f0ee
Wojtek <wojciech.nokielski@gmail.com>
2020-05-19 10:30:14 +0200

    Participated events view, some changes in project structure

---
+++ Web/joy-finder-web/src/components/Buttons.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import axios from 'axios'
+import ButtonsStyle from '../styles/ButtonsStyle.css'
+import * as Const from '../static/const';
+
+class Buttons extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            pastBG: 'transparent',
+            weekBG: 'linear-gradient(180deg, rgba(140, 232, 162, 1.0) 99.99%, rgba(140, 232, 162, 1.0) 100%)',
+            futureBG: 'transparent'
+        };
+    }
+
+    handleButtonClick = (e) => {
+        if(e.target.name == "past"){
+           this.setState({
+               pastBG: 'linear-gradient(180deg, rgba(140, 232, 162, 1.0) 99.99%, rgba(140, 232, 162, 1.0) 100%)',
+               weekBG: 'transparent',
+               futureBG: 'transparent'
+           })
+
+           if(this.props.type == "created")
+           axios.get(`${Const.API_URL}api/events/sorted/created/Past/${sessionStorage.loggedID}`  
+           )
+         .then(res => {
+             let events
+            if(res.data.error == "Not found")
+                events = '';
+            else
+                events = res.data
+            this.props.updateState('events', events)
+        })
+            else if(this.props.type == "attended")
+            axios.get(`${Const.API_URL}api/events/attended/Past/${sessionStorage.loggedID}`  
+           )
+         .then(res => {
+            let events
+            if(res.data.error == "Not found")
+                events = '';
+            else
+                events = res.data
+            this.props.updateState('events', events)
+        })
+
+        }
+
+        if(e.target.name == "week"){
+           this.setState({
+               pastBG: 'transparent',
+               weekBG: 'linear-gradient(180deg, rgba(140, 232, 162, 1.0) 99.99%, rgba(140, 232, 162, 1.0) 100%)',
+               futureBG: 'transparent'
+           })
+           if(this.props.type == "created")
+           axios.get(`${Const.API_URL}api/events/sorted/created/ThisWeek/${sessionStorage.loggedID}`  
+           )
+         .then(res => {
+            let events
+            if(res.data.error == "Not found")
+                events = '';
+            else
+                events = res.data
+            this.props.updateState('events', events)
+        })
+            else if(this.props.type == "attended")
+            axios.get(`${Const.API_URL}api/events/sorted/attended/ThisWeek/${sessionStorage.loggedID}`  
+           )
+         .then(res => {
+            let events
+            if(res.data.error == "Not found")
+                events = '';
+            else
+                events = res.data
+            this.props.updateState('events', events)
+        })
+
+        }
+
+        if(e.target.name == "future"){
+           this.setState({
+               pastBG: 'transparent',
+               weekBG: 'transparent',
+               futureBG: 'linear-gradient(180deg, rgba(140, 232, 162, 1.0) 99.99%, rgba(140, 232, 162, 1.0) 100%)'
+           })
+
+           if(this.props.type == "created")
+           axios.get(`${Const.API_URL}api/events/sorted/created/ThisYear/${sessionStorage.loggedID}`  
+           )
+         .then(res => {
+            let events
+            if(res.data.error == "Not found")
+                events = '';
+            else
+                events = res.data
+            this.props.updateState('events', events)
+        })
+            else if(this.props.type == "attended")
+            axios.get(`${Const.API_URL}api/events/sorted/attended/ThisYear/${sessionStorage.loggedID}`  
+           )
+         .then(res => {
+            let events
+            if(res.data.error == "Not found")
+                events = '';
+            else
+                events = res.data
+            this.props.updateState('events', events)
+        })
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+            <button className="past-button" name="past" onClick={this.handleButtonClick.bind(this)} style={{background: this.state.pastBG}} >Past</button>
+            <button className="week-button" name="week" onClick={this.handleButtonClick.bind(this)} style={{background: this.state.weekBG}}>Week</button>
+            <button className="future-button" name="future" onClick={this.handleButtonClick.bind(this)} style={{background: this.state.futureBG}}>Future</button>
+            </div>
+        );
+    }
+
+}
+
+export default Buttons
--- Web/joy-finder-web/src/pages/OthersEvents.js
+++ Web/joy-finder-web/src/pages/OthersEvents.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import axios from 'axios'
+import * as Const from '../static/const';
+import EventGridList from '../components/EventGridList';
+import Buttons from '../components/Buttons'
 
 class OthersEvents extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            events: []
+        };
+        this.updateState = this.updateState
+    }
+
+    componentDidMount() {
+        axios.get(`${Const.API_URL}api/events/sorted/attended/ThisWeek/${sessionStorage.loggedID}`  
+            )
+          .then(res => {
+             const events = res.data
+             console.log(res.data)
+             this.setState({
+               events: events
+           })
+         })
+     }
+
+    updateState = (name, value) => {
+        this.setState({[name]: value})
+    }
+   
     render(){
         return (
             <div className="component-background">
-                <div className="home-page-title-text">Events in which you participate!</div>
+                <div className="home-page-title-text">Your Events!</div>
+                <div>
+                        <EventGridList data={this.state}/>
+                        <Buttons date={this.state} updateState={this.updateState} type="attended"/>
+                </div>
             </div>   
             
         );
