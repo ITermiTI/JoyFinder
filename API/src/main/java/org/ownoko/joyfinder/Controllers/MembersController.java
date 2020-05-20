292ba7f3974d1355fadd64c642b7cf31d75724be
ITermiTI <56652693+ITermiTI@users.noreply.github.com>
2020-05-20 18:17:13 +0200

    CheckIfUserParticipate endpoint

--- API/src/main/java/org/ownoko/joyfinder/Controllers/MembersController.java
+++ API/src/main/java/org/ownoko/joyfinder/Controllers/MembersController.java
@@ -2,12 +2,15 @@ package org.ownoko.joyfinder.Controllers;
 
 import org.ownoko.joyfinder.Models.MembersDto;
 import org.ownoko.joyfinder.Models.MembersEntity;
+import org.ownoko.joyfinder.Models.UsersEntity;
+import org.ownoko.joyfinder.Services.API.IEventsService;
 import org.ownoko.joyfinder.Services.API.IMembersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -30,6 +33,20 @@ public class MembersController {
     @GetMapping("/byUserId/{id}")
     public ArrayList<MembersEntity> getMembersByUserId(@PathVariable int id){
        return membersService.getMembersByUserId(id);
+    }
+
+    @GetMapping("/checkIfUserParticipate/{userId}/{eventId}")
+    public boolean checkIfUserParticipate(@PathVariable("userId") int userId, @PathVariable("eventId") int eventId){
+        boolean userParticipate = false;
+        List<MembersEntity> eventMembers = membersService.getMembersByEventId(eventId);
+        List<MembersEntity> userMemberships = membersService.getMembersByUserId(userId);
+        for (MembersEntity member: eventMembers
+             ) {
+            for(MembersEntity membership : userMemberships){
+                if(member.getId() == membership.getId()) userParticipate=true;
+            }
+        }
+        return userParticipate;
     }
 
     @GetMapping("/byId/{id}")
