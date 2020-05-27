082712191f43a01711c28425399eb44d3eb4ff9a
Swobodnenerki <61480793+Swobodnenerki@users.noreply.github.com>
2020-05-27 23:00:05 +0200

    no co za syfix

--- Mobile/joy-finder-mobile/pages/AddEventPage.js
+++ Mobile/joy-finder-mobile/pages/AddEventPage.js
@@ -7,13 +7,64 @@ import {
   StatusBar,
   Icon,
   Button,
+  TextInput,
   TouchableOpacity,
 } from "react-native";
 import { Appbar } from 'react-native-paper';
 import { addEventStyle } from "../styles/AddEventStyle";
 import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+import DatePicker from 'react-native-datepicker'
+import * as Const from '../services/Const';
+import axios from 'axios';
 
 class AddEventPage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      date:"",
+      currentDate: "",
+      time: '',
+      name: '',
+      type: '',
+      city: '',
+      street: '',
+      stNumber: '',
+    };
+    //this.submitLogin = this.submitLogin.bind(this);
+  }
+  componentDidMount() {
+    var that = this;
+    var date = new Date().getDate(); 
+    var month = new Date().getMonth() + 1; 
+    var year = new Date().getFullYear();
+    that.setState({
+      currentDate:
+        year + '/' + month + '/' + date + ' ',
+    });
+  }
+  handleAddClick(){
+    console.log(this.state.name)
+    axios.post(`${Const.API_URL}api/events`, {
+      name: this.state.name,
+      date: this.state.date,
+      time: this.state.time,
+      city: this.state.city,
+      street: this.state.street,
+      stnumber: parseInt(this.state.stNumber, 10),
+      creatorid: parseInt(sessionStorage.loggedID, 10),
+      type: this.state.type,
+
+  }).then(
+      res => {
+          axios.post(`${Const.API_URL}api/members`, {
+              userId: parseInt(sessionStorage.loggedID, 10),
+              eventId: parseInt(res.data, 10)
+          }).then(
+            this.props.navigation.navigate("YourEvents")
+          )
+      })
+  }
+
   render() {
     return (
       <View style={addEventStyle.background}>
@@ -22,6 +73,134 @@ class AddEventPage extends React.Component {
             <Appbar.BackAction onPress={() => {this.props.navigation.navigate("YourEvents");}}/>
             <Appbar.Content title="Create new event"/>
         </Appbar>
+        <View style={addEventStyle.loginBox}>
+        <View style={addEventStyle.inputSection}>
+          <MaterialIcon name="person" color="white" size={28} />
+          <TextInput
+            style={addEventStyle.input}
+            placeholder="Title"
+            onChangeText={(name) => this.setState({ name: name })}
+          />
+        </View>
+        <View style={addEventStyle.inputSection}>
+        <MaterialIcon name="today" color="white" size={28}  />
+        <DatePicker
+        style={{width: 350}}
+        date={this.state.date}
+        mode="date"
+        placeholder="Date"
+        format="YYYY-MM-DD"
+        minDate={this.state.currentDate}
+        confirmBtnText="Confirm"
+        cancelBtnText="Cancel"
+        showIcon={false}
+        customStyles={{
+          dateInput:{
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomColor: 'white',
+            borderBottomWidth:0,
+          },
+          dateText: {
+            color: 'white',
+            fontSize: 18,
+            marginLeft: 7,
+            alignSelf: 'flex-start'
+
+          },
+          placeholderText: {
+            color: 'white',
+            fontSize: 18,
+            marginLeft: 7,
+            alignSelf: 'flex-start'
+          },
+        }}
+        onDateChange={(date) => {this.setState({date: date})}}
+      />
+      </View>
+      <View style={addEventStyle.inputSection}>
+        <MaterialIcon name="today" color="white" size={28}  />
+        <DatePicker
+        style={{width: 350}}
+        date={this.state.time}
+        mode="time"
+        placeholder="Time"
+        format="HH:mm"
+        minDate={this.state.currentDate}
+        confirmBtnText="Confirm"
+        cancelBtnText="Cancel"
+        is24Hour={true}
+        showIcon={false}
+        customStyles={{
+          dateInput:{
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomColor: 'white',
+            borderBottomWidth:0,
+          },
+          dateText: {
+            color: 'white',
+            fontSize: 18,
+            marginLeft: 7,
+            alignSelf: 'flex-start'
+
+          },
+          placeholderText: {
+            color: 'white',
+            fontSize: 18,
+            marginLeft: 7,
+            alignSelf: 'flex-start'
+          },
+        }}
+        onDateChange={(time) => {this.setState({time: time})}}
+      />
+      </View>
+      <View style={addEventStyle.inputSection}>
+          <MaterialIcon name="accessibility" color="white" size={28} />
+          <TextInput
+            style={addEventStyle.input}
+            placeholder="Type of event"
+            onChangeText={(type) => this.setState({ type: type })}
+          />
+        </View>
+        <View style={addEventStyle.inputSection}>
+          <MaterialIcon name="location-city" color="white" size={28} />
+          <TextInput
+            style={addEventStyle.input}
+            placeholder="City"
+            onChangeText={(city) => this.setState({ city: city })}
+          />
+        </View>
+        <View style={addEventStyle.inputSection}>
+          <MaterialIcon name="map" color="white" size={28} />
+          <TextInput
+            style={addEventStyle.input}
+            placeholder="Street"
+            onChangeText={(street) => this.setState({ street: street })}
+          />
+        </View>
+        <View style={addEventStyle.inputSection}>
+          <MaterialIcon name="location-on" color="white" size={28} />
+          <TextInput
+            style={addEventStyle.input}
+            placeholder="Street number"
+            keyboardType='number-pad'
+            onChangeText={(stNumber) => this.setState({ stNumber: stNumber })}
+          />
+        </View>
+        <TouchableOpacity 
+        style={addEventStyle.searchButton}
+
+        ><Text style={addEventStyle.signInText}>Search</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style={addEventStyle.addButton}
+        onPress={console.log(this.state.date)}
+        ><Text style={addEventStyle.signInText}>Add</Text>
+        </TouchableOpacity>
+      </View>
 
         
       </View>  
--- Mobile/joy-finder-mobile/styles/AddEventStyle.js
+++ Mobile/joy-finder-mobile/styles/AddEventStyle.js
@@ -7,5 +7,108 @@ background: {
     flex: 1,
     alignSelf: "stretch",
 },
+loginBox: {
+    top: "5.06%",
+    left: "10%",
+    width: "80%",
+    height: "14.37%",
+    flexDirection: "column",
+    alignItems: "stretch",
+  },
+  greenCircle: {
+    position: "absolute",
+    backgroundColor: "rgba(37, 167, 65, 0.1)",
+    width: 320,
+    height: 320,
+    left: "-23.06%",
+    top: "-19.06%",
+    borderRadius: 160,
+  },
+  blueCircle: {
+    position: "absolute",
+    backgroundColor: "rgba(11, 138, 255, 0.1)",
+    width: 300,
+    height: 300,
+    left: "42.5%",
+    top: "-13.75%",
+    borderRadius: 150,
+  },
+  waitingForText: {
+    top: "24.81%",
+    left: "13.88%",
+    position: "absolute",
+    fontSize: 24,
+    width: "75%",
+    fontFamily: "Roboto",
+    color: "#FFFFFF",
+  },
+  getInSection: {
+    left: "13.88%",
+    top: "31.81%",
+  },
+  getText: {
+    fontFamily: "Roboto",
+    color: "#FFFFFF",
+    fontSize: 56,
+    position: "absolute",
+  },
+  inText: {
+    fontFamily: "Roboto",
+    color: "white",
+    fontSize: 56,
+    position: "absolute",
+  },
+  inputSection: {
+    borderBottomWidth: 2,
+    borderBottomColor: "white",
+    marginBottom: "8%",
+    flexDirection: "row",
+  },
+  input: {
+    backgroundColor: "transparent",
+    fontSize: 18,
+    color: "white",
+    height: 30,
+    marginLeft: 5,
+  },
+  addButton: {
+    marginBottom: 10,
+    height: "50%",
+    backgroundColor: '#5E2758',
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  searchButton: {
+    marginBottom: 10,
+    height: "50%",
+    backgroundColor:
+      "linear-gradient(180deg, rgba(11, 138, 255, 0.51) 99.99%, rgba(11, 108, 255, 0) 100%)",
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signInText: {
+    fontSize: 24,
+    color: "#FFFFFF",
+  },
+  noAccountSection: {
+    position: "absolute",
+    alignSelf: "center",
+    top: "83.43%",
+    flexDirection: "row",
+  },
+  noAccountText: {
+    fontFamily: "Roboto",
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  createAccountText: {
+    fontFamily: "Roboto",
+    color: "#0B8AFF",
+    fontSize: 16,
+    borderBottomColor: "#0B8AFF",
+    borderBottomWidth: 1,
+  },
   
 });
